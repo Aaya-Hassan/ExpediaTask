1536b10e0c1e1a7c445e21ea47c87a6573d70a45
waleedkhaled <waleedmini2@gmail.com>
2017-04-05 19:55:11 +0300

    9

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-require('./routes.js')(app, express);
+require('./server/routes.js')(app, express);
 
-app.use(express.static(__dirname + '/.././client'));
+app.use(express.static(__dirname + '/client'));
 
 app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), function() {
