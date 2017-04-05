488be717e55624f3c523c089a99dfd938e8b328b
waleedkhaled <waleedmini2@gmail.com>
2017-04-05 21:53:38 +0300

    pathhh

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var path = require('path')
 var app = express();
 
 require('./server/routes.js')(app, express);
 
 app.use(express.static(__dirname + '/client'));
+
+app.get('/', function(req, res){
+ 	res.sendFile(path.resolve(__dirname, '/client','index.html'))
+ })
 
 app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), function() {
--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,8 @@
 var dataController = require('./data/dataController.js');
 var data = require('./data/data.json')
-var path = require('path')
 
 module.exports = function (app, express) {
- app.get('/', function(req, res){
- 	res.sendFile(path.join(__dirname, 'index.html'))
- })
+ 
  app.get('/api/getAll',dataController.getAll);
 };
 
