f0f0a1423f58ee48eaf6963b931ce5fa346f87aa
waleedkhaled <waleedmini2@gmail.com>
2017-04-05 19:27:57 +0300

    heroku

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ require('./routes.js')(app, express);
 
 app.use(express.static(__dirname + '/.././client'));
 
-app.set('port', (process.env.PORT || 6965));
+app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
 });
