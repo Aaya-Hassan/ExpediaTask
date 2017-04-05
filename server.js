9de4b044d7f46c674916d2f409fc09a20db4cca6
waleedkhaled <waleedmini2@gmail.com>
2017-04-05 21:29:03 +0300

    gittttt

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var app = express();
 
 require('./server/routes.js')(app, express);
 
-app.use(express.static(__dirname + '/client'));
+// app.use(express.static(__dirname + '/client'));
+app.use("/", express.static(__dirname + '/client'))
 
 app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), function() {
