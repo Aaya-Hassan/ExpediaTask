ccb33595f39b5f10e3e93bd0dfd0b47d24b794a6
waleedkhaled <waleedmini2@gmail.com>
2017-04-05 22:58:41 +0300

    5

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var path = require('path')
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 var app = express();
 
 require('./server/routes.js')(app, express);
@@ -8,6 +9,9 @@ app.use(express.static(__dirname + '/client'));
 // app.use('/', function(req, res){
 //  	res.send(express.static('/client'))
 //  })
+  app.use(morgan('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), function() {
