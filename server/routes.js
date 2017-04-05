bdef822ca694d4854ab9f8c482349b0a66beccac
waleedkhaled <waleedmini2@gmail.com>
2017-04-05 20:59:22 +0300

    h

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
 var dataController = require('./data/dataController.js');
-
+var data = require('./data/data.json')
 
 module.exports = function (app, express) {
- app.get('api/getAll',dataController.getAll);
+ app.get('/api/getAll',dataController.getAll);
 };
 
