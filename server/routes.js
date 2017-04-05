495a0bfa42c0db2f8bd68128304d9415b062d402
waleedkhaled <waleedmini2@gmail.com>
2017-04-05 21:17:28 +0300

    gittt

--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,9 @@ var dataController = require('./data/dataController.js');
 var data = require('./data/data.json')
 
 module.exports = function (app, express) {
+ app.get('/', function(req, res){
+ 	res.send('backend')
+ })
  app.get('/api/getAll',dataController.getAll);
 };
 
